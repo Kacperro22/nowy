f4adb0aae0297783305c551616a6f1bd367fbd67
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-22 20:31:06 +0100

    Dodano kilka zmian

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    Scanner skaner = new Scanner(System.in);
 
         System.out.println("Hello World");
-        String name;
-        name = "Kacper";
-        System.out.println("Czesc " + name);
-        int age = 21;
+
+        System.out.println("Podaj swoje imię");
+        String name = skaner.nextLine();
+
+        System.out.println("Podaj swoj wiek");
+        int age = skaner.nextInt();
+
         System.out.println("Czesc mam na imie " + name + " i mam " + age + " lat.");
+
+
+
 
 
     }
