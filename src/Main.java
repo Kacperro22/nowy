760973cec070440b6de20c7144f236b26739da35
Kacperro22 <118756501+Kacperro22@users.noreply.github.com>
2022-11-22 20:14:40 +0100

    Dodano kilka zmian

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        String name;
+        name = "Kacper";
+        System.out.println("Czesc " + name);
+        int age = 21;
+        System.out.println("Czesc mam na imie " + name + " i mam " + age + " lat.");
+
 
     }
 }
